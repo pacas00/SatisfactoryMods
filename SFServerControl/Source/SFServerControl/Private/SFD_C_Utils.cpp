d76e6200b36ab23c4d42620816661739ecdd3972
Peter Cashel <pacas00@gmail.com>
2021-04-14 03:35:24 +1000

    SFServer Control

---
+++ SFServerControl/Source/SFServerControl/Private/SFD_C_Utils.cpp
@@ -0,0 +1,134 @@
+﻿// #include "SFD_C_Utils.h"
+//
+// #include "FGNetworkLibrary.h"
+// #include "FGPlayerController.h"
+//
+//
+// TArray<FString> USFD_C_Utils::GetPlayers(const APlayerController* PlayerController)
+// {
+//     TArray<FString> PlayersList;
+//     for (AFGPlayerController* Controller : GetConnectedPlayers(PlayerController->GetWorld())) {
+//         PlayersList.Add(Controller->PlayerState->GetPlayerName());
+//     }
+//     return PlayersList;
+// }
+//
+// FBlueprintSessionResult USFD_C_Utils::GetWorldSession(UObject * WorldContextObject)
+// {
+//     UWorld * wp = WorldContextObject->GetWorld();
+//     if (wp)
+//     {
+//         //If Dedicated, This needs to be implemented when Dedicated server exist
+//         if (UKismetSystemLibrary::IsDedicatedServer(wp))
+//         {
+//             
+//         }
+//         else if (UKismetSystemLibrary::IsServer(wp))
+//         {
+//             ULocalPlayer* LocalPlayer = wp->GetFirstLocalPlayerFromController();
+//             if (LocalPlayer)
+//             {
+//                 return UFGSessionLibrary::GetMySession(LocalPlayer);
+//             }
+//
+//         }
+//     }
+//     return FBlueprintSessionResult();
+// }
+//
+// AFGAdminInterface* USFD_C_Utils::GetAdminInterface(UObject * WorldContextObject)
+// {
+//     UWorld * wp = WorldContextObject->GetWorld();
+//
+//     if (wp)
+//     {
+//         TArray<AActor*> FoundActors;
+//         UGameplayStatics::GetAllActorsOfClass(wp, AFGAdminInterface::StaticClass(), FoundActors);
+//
+//         UE_LOG(LogSFDUtils, Display, TEXT("Number of AFGAdminInterface %d"), FoundActors.Num());
+//
+//         for (AActor* Actor : FoundActors)
+//         {
+//             if ((*Actor).IsA<AFGAdminInterface>())
+//             {
+//                 return static_cast<AFGAdminInterface*>(Actor);
+//             }
+//         }
+//     }    
+//     return nullptr;
+// }
+//
+// TArray<AFGPlayerController*> USFD_C_Utils::GetAllPlayerControllers(UObject * WorldContextObject)
+// {
+//     TArray<AFGPlayerController*> PlayersList;
+//     UWorld * wp = WorldContextObject->GetWorld();
+//
+//     if (wp)
+//     {
+//         TArray<AActor*> FoundActors;
+//         UGameplayStatics::GetAllActorsOfClass(wp, AFGPlayerController::StaticClass(), FoundActors);
+//         for (AActor* Actor : FoundActors)
+//         {
+//             if ((*Actor).IsA<AFGPlayerController>())
+//             {
+//                 PlayersList.Add((static_cast<AFGPlayerController*>(Actor)));
+//             }
+//         }
+//         
+//         return PlayersList;
+//     }
+//     
+//     return PlayersList;
+// }
+//
+// AFGPlayerController* USFD_C_Utils::GetPlayerController(UObject * WorldContextObject, FString PlayerName)
+// {
+//     UWorld * wp = WorldContextObject->GetWorld();
+//
+//     if (wp)
+//     {
+//         TArray<AActor*> FoundActors;
+//         UGameplayStatics::GetAllActorsOfClass(wp, AFGPlayerController::StaticClass(), FoundActors);
+//         for (AActor* Actor : FoundActors)
+//         {
+//             if ((*Actor).IsA<AFGPlayerController>())
+//             {
+//                 AFGPlayerController* Player = (static_cast<AFGPlayerController*>(Actor));
+//                 if (Player->PlayerState)
+//                 {
+//                     
+//                     if (Player->PlayerState->GetPlayerName().ToLower() == PlayerName.ToLower())
+//                     {
+//                         return Player;
+//                     }
+//                 }
+//             }
+//         }
+//         
+//
+//     }
+//     
+//     return nullptr;
+// }
+//
+// APawn* USFD_C_Utils::GetPlayerPawn(AFGPlayerController* PlayerController)
+// {
+//     if (PlayerController)
+//     {
+//         return PlayerController->GetPawn();
+//     }
+//     
+//     return nullptr;
+// }
+//
+//
+// TArray<AFGPlayerController*> USFD_C_Utils::GetConnectedPlayers(const UWorld* World) {
+// 	TArray<AFGPlayerController*> Result;
+// 	//iterate connected players
+// 	for (FConstPlayerControllerIterator iterator = World->GetPlayerControllerIterator(); iterator; ++iterator) {
+// 		APlayerController* controller = (*iterator).Get();
+// 		if (controller == nullptr) continue;
+// 		Result.Add(static_cast<AFGPlayerController*>(controller));
+// 	}
+// 	return Result;
+//    }
---
+++ SFServerControl/Source/SFServerControl/Public/HTTPClient.h
@@ -0,0 +1,99 @@
+﻿#pragma once
+
+#include "Runtime/Online/HTTP/Public/Http.h"
+#include "HTTPClient.generated.h"
+
+UENUM()
+enum EHttpRequestStatusEnum
+{
+	NotStarted,
+    Processing,
+    Failed,
+    Failed_ConnectionError,
+    Succeeded,
+};
+
+UENUM()
+enum class ERequestResultState : uint8
+{
+	SUCCESS,
+    FAILURE,
+};
+
+USTRUCT()
+struct FRequestStruct
+{
+	GENERATED_BODY()
+public:
+    
+	UPROPERTY(EditAnywhere)
+	FString URL;
+    
+	UPROPERTY(EditAnywhere)
+	TArray<FString> Headers;
+    
+	UPROPERTY(EditAnywhere)
+	FString Verb;
+    
+	UPROPERTY(EditAnywhere)
+	TEnumAsByte<EHttpRequestStatusEnum> Status;
+};
+
+USTRUCT()
+struct FResponseStruct
+{
+	GENERATED_BODY()
+public:
+    
+	UPROPERTY(EditAnywhere)
+	int32 ResponseCode;
+    
+	UPROPERTY(EditAnywhere)
+	FString URL;
+    
+	UPROPERTY(EditAnywhere)
+	TArray<FString> Headers;
+    
+	UPROPERTY(EditAnywhere)
+	FString ContentString;
+
+	UPROPERTY(EditAnywhere)
+	FString ContentType;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FHTTPResponseReceived, const FRequestStruct&, _Request, const FResponseStruct&, _Response, const bool, _bRequestCompleted, const ERequestResultState&, RequestResultState);
+
+
+DECLARE_LOG_CATEGORY_EXTERN(LogHTTPClient, Log, Log);
+
+
+UCLASS()
+class UHTTPClient : public UActorComponent
+{
+	GENERATED_UCLASS_BODY()
+public:
+	FHttpModule* Http;
+    
+	/*Assign this function to call when the GET request processes sucessfully*/
+	void OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bRequestCompleted);
+
+
+	UFUNCTION()
+    void HttpGETCall(FString URL);
+
+	UFUNCTION()
+    void HttpPOSTCall(FString URL, FString Payload = FString(TEXT("{}")));
+
+	UPROPERTY()
+	FHTTPResponseReceived OnReceivedResponse;
+
+
+	UFUNCTION()
+    static void LogRequestToSML(FRequestStruct Request);
+
+	UFUNCTION()
+    static void LogResponseToSML(FResponseStruct Response);
+
+	UFUNCTION()
+    static bool IsJsonContentUTF8(FResponseStruct Response);
+};
